eaa21d09aed695119d9d8759a27c955173c83c4a
Jaime Jean Claude Quintero Arreola <jaime.quintero@softtek.com>
2016-08-05 12:15:15 -0700

    register works

--- app/pods/register/controller.js
+++ app/pods/register/controller.js
@@ -1,28 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  firebase: Ember.inject.service(),
-
-  model(){
-    isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
-    isPasswordValid: Ember.computed.match('password', /^[a-zA-Z0-9]{5,}$/),
-    isValid: Ember.computed.and('isEmailValid','isPasswordValid'),
-    isDisabled : Ember.computed.not('isValid'),
-  },
+  firebaseApp: Ember.inject.service(),
+  isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  isPasswordValid: Ember.computed.match('password',/^[a-zA-z0-9]+$/),
+  isValid: Ember.computed.and('isEmailValid', 'isPasswordValid'),
+  isDisabled: Ember.computed.not('isValid'),
   actions: {
     register() {
-      let controller = this;
-      this.get('firebase').createUser({
-        email: this.get('email') || '',
-        password: this.get('password') || '',
-      }, (error, data) => {
-        if (error) {
-          console.log(error);
-        } else {
-          controller.set('email', null);
-          controller.set('password', null);
-        }
+      var _that = this;
+      const auth = this.get('firebaseApp').auth();
+      const email = this.get('email');
+      const pass = this.get('password');
+      console.log(email,pass);
+      auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
+      const user = this.store.createRecord('user', {
+        id: userResponse.uid,
+        email: userResponse.email
       });
+      this.set('email', '');
+      this.set('password', '');
+      return user.save().then((response)=>{
+        _that.set('responseMessage',`Register successful with id: ${response.get('id')}`);
+        _that.set('emailAddress', '');
+        _that.set('password','');
+      });
+    });
     }
   }
 });
